06eefba4a8c817084d64eac9a8f793e120e11be5
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2023-08-30 08:54:20 +0900

    Add Observer
    [ Added ]
     Condition, Containe Observer 추가.

---
+++ Observer/GamepalyTagContaineObserver.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace StudioScor.GameplayTagSystem
+{
+    public class GamepalyTagContaineObserver : GameplayTagObserver
+    {
+        [Header(" [ Gameplay Tag Containe Observer ] ")]
+        [SerializeField] private FGameplayTags containeTags;
+
+        public GamepalyTagContaineObserver()
+        {
+
+        }
+        public GamepalyTagContaineObserver(GameObject owner, FGameplayTags containTags) : base(owner)
+        {
+            this.containeTags = containTags;
+        }
+        public GamepalyTagContaineObserver(IGameplayTagSystem gameplayTagSystem, FGameplayTags containTags) : base(gameplayTagSystem)
+        {
+            this.containeTags = containTags;
+        }
+
+        protected override void EnterObserver()
+        {
+            base.EnterObserver();
+
+
+            if (containeTags.Owneds.Count > 0)
+            {
+                GameplayTagSystem.OnGrantedOwnedTag += GameplayTagSystem_OnGrantedOwnedTag;
+                GameplayTagSystem.OnRemovedOwnedTag += GameplayTagSystem_OnRemovedOwnedTag;
+            }
+
+            if (containeTags.Blocks.Count > 0)
+            {
+                GameplayTagSystem.OnGrantedBlockTag += GameplayTagSystem_OnGrantedBlockTag;
+                GameplayTagSystem.OnRemovedBlockTag += GameplayTagSystem_OnRemovedBlockTag;
+
+            }
+
+        }
+        protected override void ExitObserver()
+        {
+            base.ExitObserver();
+
+            if (GameplayTagSystem is null)
+                return;
+
+            if (containeTags.Owneds.Count > 0)
+            {
+                GameplayTagSystem.OnGrantedOwnedTag -= GameplayTagSystem_OnGrantedOwnedTag;
+                GameplayTagSystem.OnRemovedOwnedTag -= GameplayTagSystem_OnRemovedOwnedTag;
+            }
+
+            if (containeTags.Blocks.Count > 0)
+            {
+                GameplayTagSystem.OnGrantedBlockTag -= GameplayTagSystem_OnGrantedBlockTag;
+                GameplayTagSystem.OnRemovedBlockTag -= GameplayTagSystem_OnRemovedBlockTag;
+
+            }
+        }
+
+        private void GameplayTagSystem_OnGrantedOwnedTag(IGameplayTagSystem gameplayTagSystem, GameplayTag gameplayTag)
+        {
+            if(!IsOn)
+            {
+                if(containeTags.Owneds.Contains(gameplayTag))
+                {
+                    ChangedToggleState(CanActivate());
+                }
+            }
+        }
+
+
+        private void GameplayTagSystem_OnRemovedOwnedTag(IGameplayTagSystem gameplayTagSystem, GameplayTag gameplayTag)
+        {
+            if(IsOn)
+            {
+                if (containeTags.Owneds.Contains(gameplayTag))
+                    ChangedToggleState(false);
+            }
+        }
+
+
+        private void GameplayTagSystem_OnGrantedBlockTag(IGameplayTagSystem gameplayTagSystem, GameplayTag gameplayTag)
+        {
+            if (!IsOn)
+            {
+                if (containeTags.Blocks.Contains(gameplayTag))
+                {
+                    ChangedToggleState(CanActivate());
+                }
+            }
+        }
+        private void GameplayTagSystem_OnRemovedBlockTag(IGameplayTagSystem gameplayTagSystem, GameplayTag gameplayTag)
+        {
+            if (IsOn)
+            {
+                if (containeTags.Blocks.Contains(gameplayTag))
+                    ChangedToggleState(false);
+            }
+        }
+
+
+        protected override bool CanActivate()
+        {
+            if (!base.CanActivate())
+                return false;
+
+
+            if (!GameplayTagSystem.ContainAllTagsInOwned(containeTags.Owneds))
+                return false;
+
+            if (!GameplayTagSystem.ContainAllTagsInBlock(containeTags.Blocks))
+                return false;
+
+            return false;
+        }
+
+    }
+}
---
+++ Observer/GameplayTagObserver.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+using StudioScor.Utilities;
+
+namespace StudioScor.GameplayTagSystem
+{
+    public delegate void ObserverToggleHandler(GameplayTagObserver gameplayTagConditionObserver, bool isOn);
+    public class GameplayTagObserver : BaseClass
+    {
+        [field: Header(" [ GameplayTag Observer ] ")]
+        [field: SerializeField] public GameObject Owner { get; private set; }
+
+        [field: SerializeField][field: SReadOnly] public bool IsOn { get; private set; } = false;
+        [field: SerializeField][field: SReadOnly] public bool IsPlaying { get; private set; } = false;
+
+        public IGameplayTagSystem GameplayTagSystem { get; private set; }
+        public event ObserverToggleHandler OnChangedState;
+
+        public GameplayTagObserver()
+        {
+
+        }
+        public GameplayTagObserver(GameObject owner)
+        {
+            GameplayTagSystem = owner.GetGameplayTagSystem();
+            Owner = GameplayTagSystem.gameObject;
+        }
+        public GameplayTagObserver(IGameplayTagSystem gameplayTagSystem)
+        {
+            GameplayTagSystem = gameplayTagSystem;
+            Owner = GameplayTagSystem.gameObject;
+        }
+
+        public void SetOwner(GameObject owner)
+        {
+            GameplayTagSystem = owner.GetGameplayTagSystem();
+            Owner = GameplayTagSystem.gameObject;
+        }
+        public void SetOwner(IGameplayTagSystem targetGameplayTagSystem)
+        {
+            GameplayTagSystem = targetGameplayTagSystem;
+            Owner = GameplayTagSystem.gameObject;
+        }
+        public void OnObserver()
+        {
+            if (IsPlaying)
+                return;
+
+            IsPlaying = true;
+            IsOn = false;
+
+            ChangedToggleState(CanActivate());
+
+            EnterObserver();
+        }
+
+        public void EndObserver()
+        {
+            if (!IsPlaying)
+                return;
+
+            IsPlaying = false;
+
+            if (GameplayTagSystem is null)
+                return;
+
+            ExitObserver();
+        }
+
+        protected virtual void EnterObserver()
+        {
+
+        }
+        protected virtual void ExitObserver()
+        {
+
+        }
+
+
+        protected virtual bool CanActivate()
+        {
+            return true;
+        }
+
+        protected void ChangedToggleState(bool newState)
+        {
+            if (IsOn == newState)
+                return;
+
+            IsOn = newState;
+
+            Callback_OnActivated();
+        }
+
+        protected virtual void Callback_OnActivated()
+        {
+            OnChangedState?.Invoke(this, IsOn);
+        }
+    }
+}
